ab8c075f754b207d1dd0b78a421a958ab14a860a
S L B (PercevalFox) <103103954+PercevalFox@users.noreply.github.com>
2026-02-10 23:37:58 +0100

    Add Terraform configuration for a standard VM

---
+++ day1/03-modules-lab/modules/simple-vm/main.tf
@@ -0,0 +1,29 @@
+# Ce fichier définit CE QU'EST une VM standard
+variable "project_id" {}
+variable "region" {}
+variable "vm_name" {}
+variable "subnet_id" {}
+
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = "e2-micro"
+  zone         = "${var.region}-a"
+  project      = var.project_id
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnet_id
+    # Pas d'IP publique par défaut dans le module = Standard Sécu
+  }
+  
+  # On force des labels pour le reporting financier
+  labels = {
+    managed_by = "terraform"
+    module     = "simple-vm"
+  }
+}
